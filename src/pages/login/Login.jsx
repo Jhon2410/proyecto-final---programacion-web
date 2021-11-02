b2640c57e079d53b0d389838892152df3a8e132d
jhon anderson <apxanderson02@gmail.com>
2021-11-02 12:55:48 -0500

    heroku deploy

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ export default function Login(){
 
     return(
         <>
-        <div className="container mx-auto  col-6 shadow my-2 p-3 formulario-login rounded">
+        <div className="container mx-auto  col-10 col-md-6 shadow my-2 p-3 formulario-login rounded">
             <h1 className="text-center text-uppercase text-success">Login</h1>
             <form autoComplete="off" className="mx-10">
                 <input type="text" placeholder="put your email here" autoComplete="off" className="col-12  form-control text-white py-3 my-3 " />
