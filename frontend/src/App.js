c36b3a34d3f5c5c12baa7764238452e64900fa08
jhon anderson <apxanderson02@gmail.com>
2021-11-02 12:23:58 -0500

    login y registro rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/login">
+           
+            <div className="d-flex flex-column justify-content-center align-items-center shadow vh-100 contenedorLogin"  >
+            <Login /> 
+            <Link to="/" className="d-none" >
+              pagina princpial
+            </Link>           
+            </div>
+          </Route>
+          
+     
+
+          <Route path="/register">
+           
+           <div className="d-flex flex-column justify-content-center align-items-center shadow vh-100 contenedorLogin"  >
+           <Register /> 
+           <Link to="/" className="d-none" >
+             pagina princpial
+           </Link>           
+           </div>
+         </Route>
+
+         <Route path="/menu">
+
+        <h2 className="bg-dark text-center text-white fs-3 p-3">Menu</h2>
+            
+          </Route>
+          <Route path="/">
+            <Link to="/login">ir a el login</Link>
+          </Route>
+        </Switch>
+      </Router>
+
+    
+    </>
   );
 }
 
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import "./../../App.css";
+
+export default function Login(){
+
+    return(
+        <>
+        <div className="container mx-auto  col-6 shadow my-2 p-3 formulario-login rounded">
+            <h1 className="text-center text-uppercase text-success">Login</h1>
+            <form autoComplete="off" className="mx-10">
+                <input type="text" placeholder="put your email here" autoComplete="off" className="col-12  form-control text-white py-3 my-3 " />
+                <input type="password" placeholder="put your password here" className="col-12 shadow form-control text-white py-3 my-3"  />
+                
+                <input type="submit" value="iniciar session" className="btn btn-success form-control col-12 text-white py-2" />
+            </form>
+            <h6 className="text-center col-12 ">
+            <Link to="/register" >registrarme.</Link>
+            </h6>
+
+        </div>
+        </>
+    )
+}
