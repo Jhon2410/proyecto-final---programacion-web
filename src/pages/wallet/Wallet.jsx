8201d2c8991ed0043dd7c65e7086540f9253f863
jhon anderson <apxanderson02@gmail.com>
2022-02-14 16:12:30 -0500

    stars components

--- src/pages/wallet/Wallet.jsx
+++ src/pages/wallet/Wallet.jsx
@@ -1,20 +1,10 @@
+import Starts from "../componentes/Starts";
+
 export default function Wallet() {
   return (
     <div className="container bg-dark">
       <div>
-        <h6 className=" rounded text-warning text-center fs-1">
-          12557{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="46"
-            height="46"
-            fill="currentColor"
-            class="bi bi-star-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-          </svg>
-        </h6>
+       <Starts></Starts>
         <div className="controls-Money d-flex text-center justify-content-center my-3 p-3 ">
             <button className="btn btn-enviar mx-3 text-white fw-bolder">Enviar</button>
             <button className="btn btn-retirar mx-3 text-white fw-bolder">Retirar</button>
