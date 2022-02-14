04c6b34fc23c7c1e269402908c02ac4e945fd587
jhon anderson <apxanderson02@gmail.com>
2022-02-14 00:07:53 -0500

    more options to manage money

--- src/pages/wallet/Wallet.jsx
+++ src/pages/wallet/Wallet.jsx
@@ -16,9 +16,17 @@ export default function Wallet() {
           </svg>
         </h6>
         <div className="controls-Money d-flex text-center justify-content-center my-3 p-3 ">
-            <button className="btn btn-enviar mx-3 text-white">Enviar</button>
-            <button className="btn btn-retirar mx-3 text-white">Enviar</button>
-            <button className="btn btn-historial mx-3 text-white">Enviar</button>
+            <button className="btn btn-enviar mx-3 text-white fw-bolder">Enviar</button>
+            <button className="btn btn-retirar mx-3 text-white fw-bolder">Retirar</button>
+            <button className="btn btn-info mx-3 text-white fw-bolder">Recargar</button>
+            <button className="btn btn-historial mx-3 text-white fw-bolder">Apostar</button>
+            <button className="btn btn-warning mx-3 text-white fw-bolder">prestar</button>
+        </div>
+
+        <div className="historial">
+            <div className="hitorial-item">
+            
+            </div>
         </div>
       </div>
     </div>
